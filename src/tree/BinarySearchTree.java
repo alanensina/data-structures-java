881e86adc376c243273c0ff93a6c606c884f950a
Alan Ensina <alanvinicius.ce@gmail.com>
2025-04-29 11:51:02 +0100

    BST: insert and contains added.

--- src/tree/BinarySearchTree.java
+++ src/tree/BinarySearchTree.java
@@ -2,7 +2,7 @@ package tree;
 
 public class BinarySearchTree {
 
-    private Node root;
+    public Node root;
 
     class Node{
         int value ;
@@ -14,7 +14,54 @@ public class BinarySearchTree {
         }
     }
 
-    public Node getRoot() {
-        return root;
+    public boolean insert(int value){
+        Node newNode = new Node(value);
+
+        if(this.root == null){
+            this.root = newNode;
+            return true;
+        }else{
+
+            Node temp = this.root;
+
+            while(true){
+                if(newNode.value == temp.value) return false;
+
+                if(newNode.value < temp.value){
+                    if(temp.left == null){
+                        temp.left = newNode;
+                        return true;
+                    }
+                    temp = temp.left;
+
+                }else{
+                    if(temp.right == null){
+                        temp.right = newNode;
+                        return true;
+                    }
+                    temp = temp.right;
+                }
+            }
+        }
+    }
+
+    public boolean contains(int value){
+
+        if(this.root == null) return false;
+
+        Node temp = this.root;
+
+        while(temp != null){
+            if(value < temp.value){
+                temp = temp.left;
+            }
+            else if(value > temp.value){
+                temp = temp.right;
+            }else{
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- src/tree/Main.java
+++ src/tree/Main.java
@@ -5,6 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         BinarySearchTree bst = new BinarySearchTree();
-        System.out.println(bst.getRoot());
+
+        bst.insert(47);
+        bst.insert(21);
+        bst.insert(76);
+        bst.insert(18);
+        bst.insert(52);
+        bst.insert(82);
+        bst.insert(27);
+
+        System.out.println(bst.root.left.right.value); // Check if the value 27 was added.
+
+        System.out.println(bst.contains(18)); // true
+        System.out.println(bst.contains(89)); // false
+
     }
 }
